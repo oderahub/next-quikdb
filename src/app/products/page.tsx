cb44e840eaf66e9c761c6e8f944045bd995e6909
oderahub <okere_chidera@yahoo.com>
2025-03-22 23:10:18 +0100

    chore: learning

---
+++ src/app/products/page.tsx
@@ -0,0 +1,10 @@
+const productsPage = () => {
+  return (
+    <div>
+      <h1 className="bg-blue-500 text-4xl">Products</h1>
+      <p className="text-gray-500">No products found.</p>
+    </div>
+  )
+}
+
+export default productsPage
